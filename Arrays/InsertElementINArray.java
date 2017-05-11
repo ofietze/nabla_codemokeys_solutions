c5ac3f4a0492cd1dd69e6d90020d96ad31547a3d
7Z0nE <onlyknoppas@gmail.com>
2017-05-11 10:56:28 +0200

    Update InsertElementINArray.java

--- Arrays/InsertElementINArray.java
+++ Arrays/InsertElementINArray.java
@@ -1,22 +1,26 @@
 {
-    Listobject<T>[] ary = new Listobject[getArray().length +1];
-    boolean isIn = false;
-    for (int i = 0; i < getArray().length; i++) {
-       
-        if (element.compareTo(getArray()[i]) == 0) {
-            isIn = true;
-            ary[i] = getArray()[i];
-        }
-       
-        
-        if (isIn) {
-            ary[i+1] = getArray()[i];
-        }
-        else {
-            ary[i] = getArray()[i];
-        }
-    }
+    if(element == null) return getArray();
     
-    setArray(ary);
-    return ary;
+    Listobject<T>[] arr = getArray(), newArr = new Listobject[arr.length+1];
+    int shiftFrom = arr.length;
+    for(int i = 0; i < arr.length; i++) {
+        
+        int compVal = element.compareTo(arr[i]);
+        
+        if(compVal <= 0) {
+            shiftFrom = i;
+            break;
+        }
+        else newArr[i] = arr[i];
+        
+    }
+    newArr[shiftFrom] = element;
+    for(int i = shiftFrom+1; i < newArr.length; i++)
+        newArr[i] = arr[i-1];
+    
+    setArray(newArr);
+    
+    return newArr;
 }
+
+
