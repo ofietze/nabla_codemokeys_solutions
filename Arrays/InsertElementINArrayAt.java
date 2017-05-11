fb812dc3411da7ddcdfd9313d1782562937732b8
7Z0nE <onlyknoppas@gmail.com>
2017-05-11 10:55:10 +0200

    Update InsertElementINArrayAt.java

--- Arrays/InsertElementINArrayAt.java
+++ Arrays/InsertElementINArrayAt.java
@@ -1,3 +1,28 @@
+######## FRED
+{
+    
+    Listobject<T>[] arr = getArray();
+    
+    if(pos < 0 || pos > arr.length) return arr;
+    
+    int shift = (pos == arr.length || arr[pos] != null) ? 1 : 0;
+    
+    Listobject<T>[] newArr = new Listobject[arr.length + shift];
+    
+    for(int i = 0; i < newArr.length; i++) {
+        if(i < pos) newArr[i] = arr[i];
+        else if(i == pos) newArr[i] = element;
+        else newArr[i] = arr[i - shift];
+    }
+    setArray(newArr);
+    return newArr;
+}
+
+
+
+
+
+########LUKAS
 {
     Listobject<T>[] array = getArray();
     Listobject<T>[] rtn = new Listobject[array.length + 1];
