33289298af5e6fb51ef816e7d2172ac05b9ef034
Luro <lukasroehr@users.noreply.github.com>
2017-05-11 10:55:57 +0200

    Update InsertElementINArrayAt.java

--- Arrays/InsertElementINArrayAt.java
+++ Arrays/InsertElementINArrayAt.java
@@ -17,37 +17,3 @@
     setArray(newArr);
     return newArr;
 }
-
-
-
-
-
-########LUKAS
-{
-    Listobject<T>[] array = getArray();
-    Listobject<T>[] rtn = new Listobject[array.length + 1];
-    boolean lock = false;
-    for (int i = 0; i < array.length; i++) {
-        if (i < pos) {
-            rtn[i] = array[i];
-        }
-        if (i == pos) {
-            if (array[i] == null) {
-                lock = true;
-                rtn[i] = element;
-            } else {
-                rtn[i] = element;
-                rtn[i + 1] = array[i];
-            }
-        }
-        if (i > pos) {
-            if (lock) {
-                rtn[i] = array[i];
-            } else {
-                rtn[i + 1] = array[i];
-            }
-        }
-    }
-    setArray(rtn);
-    return rtn;
-}
