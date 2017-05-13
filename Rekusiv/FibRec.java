57e5cd9662c307f4d10aebfbdb89eaeb4e2d75ea
Maurice Kraus <maurice-kraus@web.de>
2017-05-13 12:43:02 +0200

    updated FibRec

--- Rekusiv/FibRec.java
+++ Rekusiv/FibRec.java
@@ -1,5 +1,6 @@
-{
-    if (i <= 0) return 0;
-    if (i == 1) return 1;
-    return fibRec(i-1) + fibRec(i-2);
-}
+public int fibRec(int i) {
+        if (i < 0) {
+            return 0;
+        }
+        return (i == 0) ? 0 : i == 1 ? 1 : fibRec(i - 1) + fibRec(i - 2);
+    }
